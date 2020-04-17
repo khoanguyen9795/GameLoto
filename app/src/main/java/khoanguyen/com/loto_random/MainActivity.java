a9cb681fbeb53fcb40f636a9cfd64446972900de
khoanguyen9795 <khoanguyen438@gmail.com>
2020-04-17 21:15:12 +0700

    random

--- app/src/main/java/khoanguyen/com/loto_random/MainActivity.java
+++ app/src/main/java/khoanguyen/com/loto_random/MainActivity.java
@@ -3,12 +3,29 @@ package khoanguyen.com.loto_random;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Range;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-
+ EditText mEdtSobatdau,mEdtSoketthuc;
+ Button mBtnRandom;
+ TextView mTvKetqua;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mEdtSobatdau = findViewById(R.id.edittextSobatdau);
+        mEdtSobatdau = findViewById(R.id.edittextSoketthuc);
+        mBtnRandom =  findViewById(R.id.buttonRandom);
+        mTvKetqua = findViewById(R.id.textviewKetqua);
+        Random random = new Random();
+        for (int i =0;i<10;i++){
+        int value = random.nextInt(7)+5;
+        Log.d("BBB",value+"");}
     }
 }
